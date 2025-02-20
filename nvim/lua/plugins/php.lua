656bfa4574632cc341ae549e899b9690a4748e2b
manelgavalda <manelgavalda1@gmail.com>
2025-02-20 14:33:56 +0100

    Nvim avante and kitty size

--- nvim/lua/plugins/php.lua
+++ nvim/lua/plugins/php.lua
@@ -99,4 +99,49 @@ return {
       })
     end,
   },
+  {
+    "yetone/avante.nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      {
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = { file_types = { "markdown", "Avante" } },
+        ft = { "markdown", "Avante" },
+      },
+    },
+    build = "make",
+    opts = {
+      provider = "copilot",
+      behaviour = {
+        enable_token_counting = false,
+      },
+      windows = {
+        position = "right", -- the position of the sidebar
+        wrap = true,
+        width = 40,
+        sidebar_header = {
+          enabled = true, -- true, false to enable/disable the header
+          align = "center", -- left, center, right for title
+          rounded = true,
+        },
+        input = {
+          prefix = "> ",
+          height = 1, -- Adjusted height of the input window in vertical layout
+        },
+        edit = {
+          border = "rounded",
+          start_insert = true, -- Start insert mode when opening the edit window
+        },
+        ask = {
+          floating = false, -- Open the 'AvanteAsk' prompt in a floating window
+          start_insert = true, -- Start insert mode when opening the ask window
+          border = "rounded",
+          focus_on_apply = "ours", -- which diff to focus after applying
+        },
+      },
+    },
+  },
 }
