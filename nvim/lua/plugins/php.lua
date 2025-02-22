321764d9336f99c6a161fcf9f0218ad8b9c2d469
manelgavalda <manelgavalda1@gmail.com>
2025-02-22 18:46:57 +0100

    remove comment

--- nvim/lua/plugins/php.lua
+++ nvim/lua/plugins/php.lua
@@ -120,7 +120,7 @@ return {
       },
       windows = {
         position = "right",
-        width = 45, -- default % based on available width in vertical layout
+        width = 45,
         sidebar_header = {
           align = "left",
           rounded = false,
